55e7b31cc4df236d600ddfddb65ea160ae8147bf
MrFackry <daniel.d.arcos05gmail.com>
2026-04-13 16:01:28 -0500

    🐛fix: pequeños arreglos

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ import { tarjetasCollection } from './fireBase.js';
 import { crearTarjeta, crearTarjetaEnDOM } from './proyecto.js';
 import { mostrarWidgetDeSubida } from './cloudinary.js';
 import { inicializarFiltros } from './filtors.js';
-import './auth.js';
+import './Auth.js';
 
 // Esperar a que el DOM esté completamente cargado antes de ejecutar el código
 document.addEventListener('DOMContentLoaded', async () => {
